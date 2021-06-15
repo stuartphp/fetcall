ac2285c30083232b8fbd4520f5307f8aaac46b85
stuartphp <stuart@ole.co.za>
2021-06-15 11:37:32 +0200

    update

--- app/Http/Livewire/Admin/Emails.php
+++ app/Http/Livewire/Admin/Emails.php
@@ -3,18 +3,11 @@
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
-use Livewire\WithPagination;
-use App\Models\EmailAccount;
 
 class Emails extends Component
 {
-    use WithPagination;
-
     public function render()
     {
-        $data = EmailAccount::where('user_id', auth()->user()->id)->paginate(12);
-        return view('livewire.admin.emails', [
-            'emails'=>$data
-        ]);
+        return view('livewire.admin.emails');
     }
 }
--- resources/views/email/email-accounts/index.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.app')
-@section('content')
-
-@livewire('email.email-address')
-@endsection
-
